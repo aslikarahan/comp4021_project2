c6e5131cd7b55ac759b089d2f3eb61a784040948
almen1996 <almen1996@yahoo.com.hk>
2018-04-25 00:19:52 +0800

    made a simple form for adding character

---
+++ add.php
@@ -0,0 +1,14 @@
+<?php
+$char = file_get_contents("char.json");
+$char = json_decode($char, true);
+$last_id = max(array_keys($char));
+$id = $last_id + 1;
+$char[$id]["name"] = $_POST["name"];
+	$char[$id]["house"] = $_POST["house"];
+	$char[$id]["status"] = $_POST["status"];
+	$char[$id]["patronus"] =$_POST["patronus"];
+	$char[$id]["image"] = $_POST["image"];
+	file_put_contents("char.json", json_encode($char, JSON_PRETTY_PRINT));
+	print json_encode("success");
+
+?>
