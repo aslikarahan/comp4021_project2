07e86e7e1f5d1f64b74f130f4b5f3e13ad9aaa78
Asli Karahan <aslikarahan96@gmail.com>
2018-04-26 16:46:17 +0800

    filtering characters successfully done

--- list.php
+++ list.php
@@ -1,6 +1,37 @@
 <?php
 $char = file_get_contents("char.json");
-//$char = json_decode($char, true);
-print ($char);
+$char = json_decode($char, true);
+$house = $_GET["house"];
+$status = $_GET["status"];
+$temp;
+$count=1;
+if($house!=null && $status!=null){
+  for ($x = 1; $x < max(array_keys($char)); $x++) {
+    if($char[$x]["house"]==$house && $char[$x]["status"]==$status){
+      $temp[$count]=$char[$x];
+      $count++;
+    }
+  }
+}else if($house!=null && $status==null){
+  for ($x = 1; $x < max(array_keys($char)); $x++) {
+    if($char[$x]["house"]==$house){
+      $temp[$count]=$char[$x];
+      $count++;
+    }
+  }
+}else if($house==null && $status!=null){
+  for ($x = 1; $x < max(array_keys($char)); $x++) {
+    if($char[$x]["status"]==$status){
+      $temp[$count]=$char[$x];
+      $count++;
+    }
+  }
+}else{
+  $temp= $char;
+}
+
+
+$temp= json_encode($temp, JSON_PRETTY_PRINT);
+print ($temp);
 
 ?>
