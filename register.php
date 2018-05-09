09cddc90679ca6c1993837c492c4c7919d19fcc8
Asli Karahan <aslikarahan96@gmail.com>
2018-05-09 17:03:57 +0800

    login/register background

--- register.php
+++ register.php
@@ -27,7 +27,8 @@ if(isset($_SESSION['username'])){
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
   <style>
 	body{
-		background: #555;
+		background:url('img/entrance-background.jpeg') no-repeat center top #FFF;
+		background-size:cover;
 	}
   </style>
   <script>
